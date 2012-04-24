446396b98833461265a8f1ff9ec44f8db899c569
Mark Gunnels <mark@mark-Lemur-Ultra.(none)>
2012-04-24 09:52:03 -0400

    bumped versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-cloudq-client "0.0.2-SNAPSHOT"
+(defproject clj-cloudq-client "0.0.3-SNAPSHOT"
   :description "Client for cloudq"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-http "0.4.0"]
